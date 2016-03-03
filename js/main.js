3a43e9d729f3fd8b908ab3af6bf456b0d38c1b4c
samzah <szahir@hotmail.com>
2016-03-03 15:30:53 -0500

    can't get formula

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 var letter = prompt('Give me a letter');
 
 for (i = 0; i < 10; i++) {
-
-  document.write(letter+=letter)
+  document.write(letter);
+  document.write('<br>');
+  document.write(letter+=letter);
   document.write('<br>');
 
 }

63e6ef713a50e0cb3af850f0e61e61a9f87e8ad4
samzah <szahir@hotmail.com>
2016-03-03 16:12:25 -0500

    Christian helped us

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 var letter = prompt('Give me a letter');
 
 for (i = 0; i < 10; i++) {
-  document.write(letter);
-  document.write('<br>');
-  document.write(letter+=letter);
-  document.write('<br>');
 
+
+  for (k = i; k >= 0; k--) {
+    document.write(letter);
+
+  }
+
+  document.write('<br>');
 }
 
